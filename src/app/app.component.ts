39e117ccf0cfa21232cfbeac985ad6bfee281365
tranhuuphucuong <cuong@koodaa.com>
2021-06-17 16:03:01 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { format } from 'date-fns'
-import { formatToTimeZone } from 'src/utils/timezone';
+import { Component, OnInit } from "@angular/core";
+import { format } from "date-fns";
+import { formatToTimeZone } from "src/utils/timezone";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
   date = new Date();
-  year = '';
-  month = '';
-  day = '';
-  hour = '';
-  minute = '';
-  second = '';
-  milliseconds = '';
+  year = "";
+  month = "";
+  day = "";
+  hour = "";
+  minute = "";
+  second = "";
+  milliseconds = "";
 
   dateTime = {};
   dateTimeUTC = {};
   dateTimeLocal = {};
 
-  dateTimeCustom = 'abcd';
-
+  dateTimeCustom = "abcd";
 
   timestamp = Date.now();
 
   ngOnInit() {
     setInterval(() => {
-
       const now = new Date();
-      this.dateTimeCustom = formatToTimeZone(
-        now,
-        'yyyy MM dd, HH:mm:ss.SSS',
-        {timeZone: 'Asia/Tokyo'}
-      )
-      
-      this.year = format(now, 'yyyy');
-      this.month = format(now, 'MM');
-      this.day = format(now, 'dd');
-      this.hour = format(now, 'HH');
-      this.minute = format(now, 'mm');
-      this.second = format(now, 'ss');
-      this.milliseconds = format(now, 'SSS');
+      this.dateTimeCustom = formatToTimeZone(now, "yyyy MM dd, HH:mm:ss.SSS", {
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      });
+
+      this.year = format(now, "yyyy");
+      this.month = format(now, "MM");
+      this.day = format(now, "dd");
+      this.hour = format(now, "HH");
+      this.minute = format(now, "mm");
+      this.second = format(now, "ss");
+      this.milliseconds = format(now, "SSS");
       this.timestamp = now.getTime();
 
       this.dateTime = {
@@ -52,19 +48,19 @@ export class AppComponent implements OnInit {
         minute: this.minute,
         second: this.second,
         milliseconds: this.milliseconds,
-        timezone: 'local'
-      }
+        timezone: "local",
+      };
 
       this.dateTimeUTC = {
-        year: formatToTimeZone(now, 'yyyy', {timeZone: 'UTC'}),
-        month: formatToTimeZone(now, 'MM' , {timeZone: 'UTC'}),
-        day: formatToTimeZone(now, 'dd', {timeZone: 'UTC'}),
-        hour: formatToTimeZone(now, 'HH', {timeZone: 'UTC'}),
-        minute: formatToTimeZone(now, 'mm', {timeZone: 'UTC'}),
-        second: formatToTimeZone(now, 'ss', {timeZone: 'UTC'}),
-        milliseconds: formatToTimeZone(now, 'SSS', {timeZone: 'UTC'}),
-        timezone: 'UTC'
-      }
+        year: formatToTimeZone(now, "yyyy", { timeZone: "UTC" }),
+        month: formatToTimeZone(now, "MM", { timeZone: "UTC" }),
+        day: formatToTimeZone(now, "dd", { timeZone: "UTC" }),
+        hour: formatToTimeZone(now, "HH", { timeZone: "UTC" }),
+        minute: formatToTimeZone(now, "mm", { timeZone: "UTC" }),
+        second: formatToTimeZone(now, "ss", { timeZone: "UTC" }),
+        milliseconds: formatToTimeZone(now, "SSS", { timeZone: "UTC" }),
+        timezone: "UTC",
+      };
 
       this.dateTimeLocal = {
         year: this.year,
@@ -74,9 +70,9 @@ export class AppComponent implements OnInit {
         minute: this.minute,
         second: this.second,
         milliseconds: this.milliseconds,
-        timezone: null
-      }
-    }, 500)
+        timezone: null,
+      };
+    }, 500);
   }
 
   // f() {
